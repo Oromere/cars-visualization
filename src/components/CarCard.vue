c89c7595fe0772a2fd6f47d4e288ba1ae473daba
Florian <flo.schlueter.dev@gmail.com>
2020-06-10 14:19:36 +0200

    init, get data

---
+++ src/components/CarCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CarCard'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    chainWebpack: config => {
+      config.module
+        .rule('csv')
+        .test(/\.csv$/)
+        .use('csv-loader')
+          .loader('csv-loader')
+          .tap(options => ({
+            dynamicTyping: true,
+            header: true,
+            skipEmptyLines: true
+          }))
+          .end()
+    }
+}
